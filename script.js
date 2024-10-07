9a48a8f6b3222cfb88ea98deb3233b6e277d3367
Nawaf almehdhar <181556586+almehdharHub@users.noreply.github.com>
2024-10-08 02:38:34 +0300

    toggle options

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-let options = document.querySelectorAll("button");
+let firstOptions = document.querySelectorAll(".first-options");
+let secondOptions = document.querySelectorAll(".second-options");
+let buttons = document.querySelectorAll("button");
 
 let result = document.querySelector("#result");
 
-options.forEach((option) => {
+buttons.forEach((option) => {
   option.addEventListener("click", () => {
-    result.innerHTML = option.innerHTML;
+    if (option.classList.contains("first-options")) {
+      console.log("first options");
+      firstOptions.forEach((option) => {
+        option.classList.add("d-none");
+        console.log("first options");
+      });
+      secondOptions.forEach((option) => {
+        option.classList.remove("d-none");
+        console.log("first options", option);
+      });
+    } else if (option.classList.contains("second-options")) {
+      firstOptions.forEach((option) => {
+        option.classList.remove("d-none");
+      });
+      secondOptions.forEach((option) => {
+        option.classList.add("d-none");
+      });
+    }
+
+    result.innerHTML += option.innerHTML;
   });
 });
